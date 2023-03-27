d0bba20c8526daec8b7c18b2ea5006a59f9c0362
josef <macerajosef@gmail.com>
2023-03-27 11:10:09 -0700

    feat(api): use global validation pipes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
+import { ZodValidationPipe } from 'nestjs-zod';
 import { ApiModule } from './api/api.module';
 import { UsersModule } from './users/users.module';
 import { AuthenticationModule } from './authentication/authentication.module';
@@ -7,6 +9,11 @@ import { AuthorizationModule } from './authorization/authorization.module';
 @Module({
   imports: [ApiModule, UsersModule, AuthenticationModule, AuthorizationModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ZodValidationPipe,
+    },
+  ],
 })
 export class AppModule {}
