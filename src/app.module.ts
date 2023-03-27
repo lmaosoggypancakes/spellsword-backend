e00e7ff96cc4dc4fc6e069221c4a479065713657
josef <macerajosef@gmail.com>
2023-03-26 21:46:32 -0700

    feat: add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ApiModule } from './api/api.module';
+import { UsersModule } from './users/users.module';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { AuthorizationModule } from './authorization/authorization.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ApiModule, UsersModule, AuthenticationModule, AuthorizationModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
