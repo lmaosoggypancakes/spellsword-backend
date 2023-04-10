b8014683e5e838f3b6b1c70fa857efe39b7cc850
lmaosoggypancakes <macerajosef@gmail.com>
2023-04-09 21:52:09 -0700

    chore: remove unused imports

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from 'src/auth/auth.module';
-import { PrismaService } from 'src/prisma.service';
 import { UsersModule } from 'src/users/users.module';
-import { UsersService } from 'src/users/users.service';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
 
