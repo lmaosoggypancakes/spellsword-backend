a210ccc58d7271f8e9c1ad7afa749ff72f49318e
lmaosoggypancakes <macerajosef@gmail.com>
2023-04-09 21:52:36 -0700

    fix: use ConfigModule for env handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,16 @@ import { ZodValidationPipe } from 'nestjs-zod';
 import { ApiModule } from './api/api.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { GameGateway } from './gateways/play.gateway';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [ApiModule, UsersModule, AuthModule],
-  controllers: [],
+  imports: [ConfigModule.forRoot(), ApiModule, UsersModule, AuthModule],
   providers: [
     {
       provide: APP_PIPE,
       useClass: ZodValidationPipe,
     },
+    GameGateway,
   ],
 })
 export class AppModule {}
