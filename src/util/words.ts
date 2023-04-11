c5840fc5196dea6196572cecaf2927d5d7783cd4
lmaosoggypancakes <macerajosef@gmail.com>
2023-04-10 23:06:43 -0700

    feat: add random word list generation

---
+++ src/util/words.ts
@@ -0,0 +1,21 @@
+export const consonants = 'BCDFGHJKLMNPQRSTVWXYZ';
+export const vowels = 'AEIOU';
+export const alphabet = consonants + vowels;
+export const numVowels = 3;
+export const numConsonants = 5;
+export const generateRandomSequence = () => {
+  const randomVowels = [...Array(numVowels)].map(
+    () => vowels[Math.floor(Math.random() * vowels.length)],
+  );
+  const randomConsonants = [...Array(numConsonants)].map(
+    () => consonants[Math.floor(Math.random() * consonants.length)],
+  );
+  console.log(randomVowels);
+  const seq = randomVowels.concat(randomConsonants);
+  return seq.join('');
+};
+
+export const verifyWord = async (word: string): Promise<boolean> => {
+  // TODO: use dictionary API to verify word
+  return false;
+};
