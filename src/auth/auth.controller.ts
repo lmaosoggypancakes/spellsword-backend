68455042aef005e0e1c030373418fd6ae56ba196
lmaosoggypancakes <macerajosef@gmail.com>
2023-04-12 23:25:20 -0700

    feat: add hello testing route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { JwtAuthGuard } from './guards';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
@@ -8,5 +9,11 @@ export class AuthController {
   @Post('login')
   async login(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('hello')
+  async hello(@Request() req) {
+    return req.user;
   }
 }
